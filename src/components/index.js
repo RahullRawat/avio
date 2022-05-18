69a3098896047574ed15a0f2ba133937db3ea2fa
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-18 15:29:10 +0530

    feat : export components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { Navbar } from "./Navbar/Navbar";
+export { Sidebar } from "./Sidebar/Sidebar";
+export { RightSidebar } from "./RightSidebar/RightSidebar";
+export { PostModal } from "./PostModal/PostModal";
+export { PostCard } from "./PostCard/PostCard";
