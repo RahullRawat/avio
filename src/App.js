a8b38ade51a8e5aaf5fe29d71d534845d957c2cb
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-18 15:28:33 +0530

    feat : routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar } from "./components/index";
+import { Bookmark, Explore, Home, Profile } from "./pages/index";
+
 function App() {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		<div className="box-border">
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/profile" element={<Profile />}></Route>
+				<Route path="/explore" element={<Explore />}></Route>
+				<Route path="/bookmark" element={<Bookmark />}></Route>
+				<Route path="/sidebar" element={<Sidebar />}></Route>
+			</Routes>
 		</div>
 	);
 }
