4fcac1b9803535cc39e599031075c703a62b77a5
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-18 15:44:30 +0530

    fix : buttons size in explore

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -8,20 +8,20 @@ export const Explore = () => {
 				<Sidebar />
 				<div className="w-[55%] mx-auto xl:ml-[12rem] lg:w-[45%] md:ml-[6rem] md:w-[100%] sm:ml-[4rem]">
 					<h1 className="my-4 font-bold text-2xl">Explore</h1>
-					<div className="flex justify-between">
-						<button className="border border-primary text-primary text-sm px-1 py-1 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4">
+					<div className="flex">
+						<button className="border border-primary text-primary text-sm px-4 py-1 mr-2 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4 lg:px-1 sm:text-xs">
 							For You
 						</button>
-						<button className="border border-primary text-primary text-sm px-1 py-1 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4">
+						<button className="border border-primary text-primary text-sm px-4 py-1 mr-2 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4 lg:px-1 sm:text-xs">
 							Trending
 						</button>
-						<button className="border border-primary text-primary text-sm px-1 py-1 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4">
+						<button className="border border-primary text-primary text-sm px-4 py-1 mr-2 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4 lg:px-1 sm:text-xs">
 							Sports
 						</button>
-						<button className="border border-primary text-primary text-sm px-1 py-1 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4">
+						<button className="border border-primary text-primary text-sm px-4 py-1 mr-2 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4 lg:px-1 sm:text-xs">
 							Technology
 						</button>
-						<button className="border border-primary text-primary text-sm px-1 py-1 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4">
+						<button className="border border-primary text-primary text-sm px-4 py-1 mr-2 mt-4 font-medium hover:bg-primary hover:text-white rounded-md mb-4 lg:px-1 sm:text-xs">
 							News
 						</button>
 					</div>
