ff9efa8a9e58ec0cce0888be18f6a66e1af3d805
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-19 17:30:36 +0530

    feat : authSlice

---
+++ src/redux/slices/authSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { logIN, signUp } from "../thunks/authThunk";
+
+const initialState = {
+	user: JSON.parse(localStorage.getItem("user")) || null,
+	token: localStorage.getItem("token") || null,
+	status: "idle",
+};
+
+console.log(initialState);
+
+const authSlice = createSlice({
+	name: "auth",
+	initialState,
+	reducers: {
+		logout: (state) => {
+			state.user = null;
+			state.token = null;
+			localStorage.removeItem("user");
+			localStorage.removeItem("token");
+		},
+	},
+	extraReducers: {
+		[logIN.pending]: (state) => {
+			state.status = "loading";
+		},
+		[logIN.fulfilled]: (state, action) => {
+			state.status = "success";
+			state.token = action.payload.data.encodedToken;
+			state.user = action.payload.data.foundUser;
+		},
+		[logIN.rejected]: (state, action) => {
+			state.status = "failed";
+		},
+		[signUp.pending]: (state) => {
+			state.status = "loading";
+		},
+		[signUp.fulfilled]: (state, action) => {
+			state.status = "success";
+			state.token = action.payload.data.encodedToken;
+			state.user = action.payload.data.foundUser;
+		},
+		[signUp.rejected]: (state, action) => {
+			state.status = "failed";
+			console.log(action);
+		},
+	},
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
